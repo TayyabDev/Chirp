37f3fae6b37104305cafca7cd65fe4f3261a27ad
Tayyab <r44zor@gmail.com>
2021-03-09 16:27:34 -0500

    add react player

--- chirp/src/components/dashboard.js
+++ chirp/src/components/dashboard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Feed from "./feed";
 import Header from "./header";
+import ReactPlayer from "react-player";
 
 export default function Dashboard(props) {
   // replaced with real props once connected to backend
@@ -12,7 +13,11 @@ export default function Dashboard(props) {
   return (
     <div>
       <Header {...p} />
-      <Feed />
+      <ReactPlayer
+        class="flex justify-center"
+        url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+        controls
+      />
     </div>
   );
 }
